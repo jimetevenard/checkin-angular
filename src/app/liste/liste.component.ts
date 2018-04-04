a6f061075a2913671383bc84afaeaf8f6fcb9b23
Jim Etevenard <jimetevenard@gmail.com>
2018-04-04 16:28:48 +0200

    Gestion des accents

--- src/app/liste/liste.component.ts
+++ src/app/liste/liste.component.ts
@@ -43,12 +43,29 @@ export class ListeComponent implements OnInit {
   }
   
   private sansAccent(str): string {
-    return str.replace(/[\u0300-\u036f]/g, "");
+    return RemoveAccents(str);
   }
+  
   
   private normStart(a, b): boolean {
     // a starts whith b en ignorant accents et casse.
     return this.sansAccent(a.toLowerCase()).startsWith(this.sansAccent(b.toLowerCase()));
   }
 
+}
+
+function RemoveAccents(strAccents) {
+  var strAccents = strAccents.split('');
+  var strAccentsOut = new Array();
+  var strAccentsLen = strAccents.length;
+  var accents = 'ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž';
+  var accentsOut = "AAAAAAaaaaaaOOOOOOOooooooEEEEeeeeeCcDIIIIiiiiUUUUuuuuNnSsYyyZz";
+  for (var y = 0; y < strAccentsLen; y++) {
+    if (accents.indexOf(strAccents[y]) != -1) {
+      strAccentsOut[y] = accentsOut.substr(accents.indexOf(strAccents[y]), 1);
+    } else
+      strAccentsOut[y] = strAccents[y];
+  }
+  var strOut = strAccentsOut.join('');
+  return strOut;
 }
