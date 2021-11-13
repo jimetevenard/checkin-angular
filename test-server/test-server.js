e8ee4a9c732df8d1c1faea7ec60b0e5c7643a659
Jim Etvenard <jimetevenard@gmail.com>
2021-11-13 15:47:53 +0100

    Serveur Test

---
+++ test-server/test-server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs = require("fs");
+
+// Fichier de test
+const TEST_FILE = "/liste-test.json";
+
+fs.readFile(__dirname + TEST_FILE, (error, data) => {
+    if (error) throw error;
+
+    const jsonData = data.toString();
+
+    let testServer = http.createServer((req, res) => {
+        switch (req.url) {
+
+            case '/list':
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(jsonData);
+                break;
+
+            case '/':
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end('<h1>Backoffice</h1><p>Page Backoffice</p>');
+                break;
+
+            default:
+                res.writeHead(404, {});
+                res.end('');
+        }
+
+
+    });
+
+    // Start the server on port 3000
+    testServer.listen(3000, '127.0.0.1');
+    console.log('Test server running on port 3000');
+});
+
